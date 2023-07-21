96b01b72c30c1e6e68f1cb16660cbc6be8fcd5a9
Tommy08589 <139736030+Tommy08589@users.noreply.github.com>
2023-07-22 00:06:04 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-  ['xxxxxxxxxx', '𝐃𝚲𝐍𝕀𝚵𝐋͎💋', true],
+  ['3314507715', '𝐃𝚲𝐍𝕀𝚵𝐋͎💋', true],
   ['xxxxxxxxxx',],
   //['xxxxxxxxxx'],	
   ['xxxxxxxxxx',],
