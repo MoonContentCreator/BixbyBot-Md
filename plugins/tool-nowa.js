e2866e5f9de918b91405419d548d3e93e9dd1014
MoonContentCreator <136917052+MoonContentCreator@users.noreply.github.com>
2023-07-31 00:17:00 +0200

    Add files via upload

---
+++ plugins/tool-nowa.js
@@ -0,0 +1,26 @@
+/*---------------------------------------------------------------------------------------
+  🍀 • By https://github.com/ALBERTO9883
+  🍀 • ⚘Alberto Y Ashly⚘
+-----------------------------------------------------------------------------------------*/
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+let regex = /x/g
+if (!text) throw 'numero mancante'
+if (!text.match(regex)) throw `esempio: ${usedPrefix + command} 39333333333x`
+let random = text.match(regex).length, total = Math.pow(10, random), array = []
+for (let i = 0; i < total; i++) {
+let list = [...i.toString().padStart(random, '0')]
+let result = text.replace(regex, () => list.shift()) + '@s.whatsapp.net'
+if (await conn.onWhatsApp(result).then(v => (v[0] || {}).exists)) {
+let info = await conn.fetchStatus(result).catch(_ => {})
+array.push({ exists: true, jid: result, ...info })
+} else {
+array.push({ exists: false, jid: result })
+}}
+let txt = 'Registrato\n\n' + array.filter(v => v.exists).map(v => `• Num: wa.me/${v.jid.split('@')[0]}\n*• Bio:* ${v.status || 'Sin descripcion'}\n*• Data:* ${formatDate(v.setAt)}`).join('\n\n') + '\n\n*Non registrato*\n\n' + array.filter(v => !v.exists).map(v => v.jid.split('@')[0]).join('\n')
+m.reply(txt)
+}
+handler.command = /^nowa$/i
+export default handler
+function formatDate(n, locale = 'id') {
+let d = new Date(n)
+return d.toLocaleDateString(locale, { timeZone: 'Asia/Jakarta' })}
