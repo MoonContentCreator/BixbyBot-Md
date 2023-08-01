cbe03304b25a54733e8c9ba7e468e382c08fe41b
D3sker <140189425+D3sker@users.noreply.github.com>
2023-08-01 10:29:49 +0200

    Bixby desker

--- config.js
+++ config.js
@@ -3,13 +3,7 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-  ['xxxxxxxxxx', '𝐃𝚲𝐍𝕀𝚵𝐋͎💋', true],
-  ['xxxxxxxxxx',],
-  //['xxxxxxxxxx'],	
-  ['xxxxxxxxxx',],
-  ['xxxxxxxxxx'],
-  ['xxxxxxxxxx'],
-] //Numero degli owner
+  ['393515638253', 'Desker'] //Numero degli owner
 
 //════════════ •⊰✧⊱• ════════════
 
