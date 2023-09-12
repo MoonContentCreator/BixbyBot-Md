6fd64052ad2a67e632f9b75908adb883fec76d54
MoonContentCreator <136917052+MoonContentCreator@users.noreply.github.com>
2023-09-12 23:26:26 +0200

    Update info-owner.js

--- plugins/info-owner.js
+++ plugins/info-owner.js
@@ -1,11 +1,1 @@
-
-function handler(m) {
-  const data = global.owner.filter(([id, isCreator]) => id && isCreator)
-  this.sendContact(m.chat, data.map(([id, name]) => [id, name]), m)
-
-}
-
-handler.help = ['owner']
-handler.tags = ['main']
-handler.command = ['proprietario', 'creador', 'dueño', 'fgowner'] 
-export default handler
+const _0x393e08=_0x5986;(function(_0x97d674,_0x370265){const _0x44dcee=_0x5986,_0x13bb36=_0x97d674();while(!![]){try{const _0x1f0a56=-parseInt(_0x44dcee(0x127))/0x1+-parseInt(_0x44dcee(0x130))/0x2*(parseInt(_0x44dcee(0x12f))/0x3)+parseInt(_0x44dcee(0x11c))/0x4*(parseInt(_0x44dcee(0x11a))/0x5)+-parseInt(_0x44dcee(0x129))/0x6+parseInt(_0x44dcee(0x128))/0x7*(parseInt(_0x44dcee(0x12d))/0x8)+-parseInt(_0x44dcee(0x11f))/0x9*(parseInt(_0x44dcee(0x11e))/0xa)+parseInt(_0x44dcee(0x120))/0xb;if(_0x1f0a56===_0x370265)break;else _0x13bb36['push'](_0x13bb36['shift']());}catch(_0x5bfc75){_0x13bb36['push'](_0x13bb36['shift']());}}}(_0x581f,0x51488));function handler(_0x57ffa3){const _0xc6befa=_0x5986;let _0x2507ec={'key':{'participants':_0xc6befa(0x12a),'fromMe':![],'id':_0xc6befa(0x122)},'message':{'extendedTextMessage':{'text':_0xc6befa(0x119),'vcard':_0xc6befa(0x118)}},'participant':_0xc6befa(0x12a)};const _0xc06d49=global[_0xc6befa(0x11d)][_0xc6befa(0x124)](([_0x38ba61,_0x437c83])=>_0x38ba61&&_0x437c83);this[_0xc6befa(0x121)](_0x57ffa3[_0xc6befa(0x12c)],_0xc06d49[_0xc6befa(0x125)](([_0x42018f,_0x1302d8])=>[_0x42018f,_0x1302d8]),_0x2507ec);}handler[_0x393e08(0x12b)]=[_0x393e08(0x11d)],handler[_0x393e08(0x12e)]=[_0x393e08(0x11b)],handler['command']=['proprietario',_0x393e08(0x123),_0x393e08(0x126),'fgowner'];function _0x581f(){const _0x28a297=['2403618kkIOrP','0@s.whatsapp.net','help','chat','6696iPmJHW','tags','3zdkLgp','452024pPdPzc','BEGIN:VCARD\x0aVERSION:3.0\x0aN:;Unlimited;;;\x0aFN:Unlimited\x0aORG:Unlimited\x0aTITLE:\x0aitem1.TEL;waid=19709001746:+1\x20(970)\x20900-1746\x0aitem1.X-ABLabel:Unlimited\x0aX-WA-BIZ-DESCRIPTION:ofc\x0aX-WA-BIZ-NAME:Unlimited\x0aEND:VCARD','𝐎𝐰𝐧𝐞𝐫\x20𝐁𝐢𝐱𝐛𝐲𝐁𝐨𝐭-𝐌𝐝','27085DXPJcB','main','440ADyhZU','owner','1790CNRxOe','8739PPFDwZ','7873184bSpQbi','sendContact','Halo','creador','filter','map','dueño','619353jwuibX','3689kioSZg'];_0x581f=function(){return _0x28a297;};return _0x581f();}function _0x5986(_0x43946b,_0x3487ed){const _0x581f4e=_0x581f();return _0x5986=function(_0x59861f,_0x3b2401){_0x59861f=_0x59861f-0x118;let _0x9a00c6=_0x581f4e[_0x59861f];return _0x9a00c6;},_0x5986(_0x43946b,_0x3487ed);}export default handler;
