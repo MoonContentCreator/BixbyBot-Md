92c5b09df1401c953dd29a37dc8b8a5cd54f1516
MoonContentCreator <136917052+MoonContentCreator@users.noreply.github.com>
2023-10-01 17:37:02 +0200

    Update gp-hidetag.js

--- plugins/gp-hidetag.js
+++ plugins/gp-hidetag.js
@@ -1,1 +1,43 @@
-function _0x2e0e(){const _0xf37617=['group','sender','jid','mimetype','69460KxVSJL','getQuotedObj','video/mp4','map','chat','1101160bCmeSM','decodeJid','relayMessage','3400HgRgzW','admin','fromCharCode','command','Hidetag.mp3','repeat','message','sendMessage','3661092imoLIC','audio/mp4','quoted','881498YHhNmj','246chsDut','33TQqOHO','mtype','cMod','download','imageMessage','audioMessage','text','2655MvENGm','116UaSdjo','77330oRyjSq','user','3815ORrZpS'];_0x2e0e=function(){return _0xf37617;};return _0x2e0e();}const _0x213341=_0x2a00;(function(_0x4873be,_0x296c16){const _0x51a1c5=_0x2a00,_0x38b145=_0x4873be();while(!![]){try{const _0x444858=-parseInt(_0x51a1c5(0x204))/0x1+-parseInt(_0x51a1c5(0x1f9))/0x2+parseInt(_0x51a1c5(0x202))/0x3*(-parseInt(_0x51a1c5(0x203))/0x4)+-parseInt(_0x51a1c5(0x1e6))/0x5*(-parseInt(_0x51a1c5(0x1fa))/0x6)+parseInt(_0x51a1c5(0x206))/0x7*(parseInt(_0x51a1c5(0x1ee))/0x8)+parseInt(_0x51a1c5(0x1f6))/0x9+parseInt(_0x51a1c5(0x1eb))/0xa*(-parseInt(_0x51a1c5(0x1fb))/0xb);if(_0x444858===_0x296c16)break;else _0x38b145['push'](_0x38b145['shift']());}catch(_0x287f13){_0x38b145['push'](_0x38b145['shift']());}}}(_0x2e0e,0x51845));import{generateWAMessageFromContent}from'@adiwajshing/baileys';function _0x2a00(_0x1c6e1b,_0x698ce4){const _0x2e0ed7=_0x2e0e();return _0x2a00=function(_0x2a00f2,_0x1330a0){_0x2a00f2=_0x2a00f2-0x1e3;let _0x47a482=_0x2e0ed7[_0x2a00f2];return _0x47a482;},_0x2a00(_0x1c6e1b,_0x698ce4);}import*as _0x48214f from'fs';let handler=async(_0x379892,{conn:_0x1db890,text:_0x2adbf9,participants:_0x520dc9,isOwner:_0x2c8f8c,isAdmin:_0x2ee66f})=>{const _0x1f950d=_0x2a00;try{let _0x4bb798=_0x520dc9[_0x1f950d(0x1e9)](_0x2f4de1=>_0x1db890[_0x1f950d(0x1ec)](_0x2f4de1['id'])),_0x4b7a57=_0x379892[_0x1f950d(0x1f8)]?_0x379892[_0x1f950d(0x1f8)]:_0x379892||_0x379892['text']||_0x379892[_0x1f950d(0x1e3)],_0x5cf866=_0x379892[_0x1f950d(0x1f8)]?await _0x379892[_0x1f950d(0x1e7)]():_0x379892['msg']||_0x379892[_0x1f950d(0x201)]||_0x379892[_0x1f950d(0x1e3)],_0x31f147=_0x1db890[_0x1f950d(0x1fd)](_0x379892[_0x1f950d(0x1ea)],generateWAMessageFromContent(_0x379892[_0x1f950d(0x1ea)],{[_0x379892[_0x1f950d(0x1f8)]?_0x4b7a57['mtype']:'extendedTextMessage']:_0x379892['quoted']?_0x5cf866[_0x1f950d(0x1f4)][_0x4b7a57['mtype']]:{'text':''||_0x5cf866}},{}),_0x2adbf9||_0x4b7a57['text'],_0x1db890[_0x1f950d(0x205)][_0x1f950d(0x1e4)],{'mentions':_0x4bb798});await _0x1db890[_0x1f950d(0x1ed)](_0x379892[_0x1f950d(0x1ea)],_0x31f147['message'],{'messageId':_0x31f147['key']['id']});}catch{let _0x3fb93d=_0x520dc9[_0x1f950d(0x1e9)](_0x1029f6=>_0x1db890[_0x1f950d(0x1ec)](_0x1029f6['id'])),_0x2d41f5=_0x379892[_0x1f950d(0x1f8)]?_0x379892[_0x1f950d(0x1f8)]:_0x379892,_0x141de6=(_0x2d41f5['msg']||_0x2d41f5)[_0x1f950d(0x1e5)]||'',_0x675f=/image|video|sticker|audio/['test'](_0x141de6),_0x97a737=String[_0x1f950d(0x1f0)](0x200e),_0x3038c1=_0x97a737[_0x1f950d(0x1f3)](0x352),_0x46eb24=''+(_0x2adbf9?_0x2adbf9:'.hidetag');if(_0x675f&&_0x2d41f5['mtype']===_0x1f950d(0x1ff)&&_0x46eb24){var _0x514bcc=await _0x2d41f5[_0x1f950d(0x1fe)]?.();_0x1db890[_0x1f950d(0x1f5)](_0x379892[_0x1f950d(0x1ea)],{'image':_0x514bcc,'mentions':_0x3fb93d,'caption':_0x46eb24,'mentions':_0x3fb93d},{'quoted':_0x379892});}else{if(_0x675f&&_0x2d41f5[_0x1f950d(0x1fc)]==='videoMessage'&&_0x46eb24){var _0x514bcc=await _0x2d41f5[_0x1f950d(0x1fe)]?.();_0x1db890['sendMessage'](_0x379892[_0x1f950d(0x1ea)],{'video':_0x514bcc,'mentions':_0x3fb93d,'mimetype':_0x1f950d(0x1e8),'caption':_0x46eb24},{'quoted':_0x379892});}else{if(_0x675f&&_0x2d41f5[_0x1f950d(0x1fc)]===_0x1f950d(0x200)&&_0x46eb24){var _0x514bcc=await _0x2d41f5[_0x1f950d(0x1fe)]?.();_0x1db890[_0x1f950d(0x1f5)](_0x379892[_0x1f950d(0x1ea)],{'audio':_0x514bcc,'mentions':_0x3fb93d,'mimetype':_0x1f950d(0x1f7),'fileName':_0x1f950d(0x1f2)},{'quoted':_0x379892});}else{if(_0x675f&&_0x2d41f5[_0x1f950d(0x1fc)]==='stickerMessage'&&_0x46eb24){var _0x514bcc=await _0x2d41f5[_0x1f950d(0x1fe)]?.();_0x1db890[_0x1f950d(0x1f5)](_0x379892[_0x1f950d(0x1ea)],{'sticker':_0x514bcc,'mentions':_0x3fb93d},{'quoted':_0x379892});}else await _0x1db890[_0x1f950d(0x1ed)](_0x379892[_0x1f950d(0x1ea)],{'extendedTextMessage':{'text':_0x3038c1+'\x0a'+_0x46eb24+'\x0a',...{'contextInfo':{'mentionedJid':_0x3fb93d,'externalAdReply':{'thumbnail':imagen1,'sourceUrl':'stocazzo'}}}}},{});}}}}};handler[_0x213341(0x1f1)]=/^(hidetag|notificar|notify)$/i,handler[_0x213341(0x207)]=!![],handler[_0x213341(0x1ef)]=!![];export default handler;
+import { generateWAMessageFromContent } from '@whiskeysockets/baileys' 
+ import * as fs from 'fs' 
+ let handler = async (m, { conn, text, participants, isOwner, isAdmin }) => { 
+ try {   
+ let users = participants.map(u => conn.decodeJid(u.id)) 
+ let q = m.quoted ? m.quoted : m || m.text || m.sender 
+ let c = m.quoted ? await m.getQuotedObj() : m.msg || m.text || m.sender 
+ let msg = conn.cMod(m.chat, generateWAMessageFromContent(m.chat, { [m.quoted ? q.mtype : 'extendedTextMessage']: m.quoted ? c.message[q.mtype] : { text: '' || c }}, {}), text || q.text, conn.user.jid, { mentions: users }) 
+ await conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id }) 
+  
+ } catch {   
+  
+ /** 
+ [ By @NeKosmic || https://github.com/NeKosmic/ ] 
+ **/   
+  
+ let users = participants.map(u => conn.decodeJid(u.id)) 
+ let quoted = m.quoted ? m.quoted : m 
+ let mime = (quoted.msg || quoted).mimetype || '' 
+ let isMedia = /image|video|sticker|audio/.test(mime) 
+ let more = String.fromCharCode(8206) 
+ let masss = more.repeat(850) 
+ let htextos = `${text ? text : ".hidetag"}` 
+ if ((isMedia && quoted.mtype === 'imageMessage') && htextos) { 
+ var mediax = await quoted.download?.() 
+ conn.sendMessage(m.chat, { image: mediax, mentions: users, caption: htextos, mentions: users }, { quoted: m }) 
+ } else if ((isMedia && quoted.mtype === 'videoMessage') && htextos) { 
+ var mediax = await quoted.download?.() 
+ conn.sendMessage(m.chat, { video: mediax, mentions: users, mimetype: 'video/mp4', caption: htextos }, { quoted: m }) 
+ } else if ((isMedia && quoted.mtype === 'audioMessage') && htextos) { 
+ var mediax = await quoted.download?.() 
+ conn.sendMessage(m.chat, { audio: mediax, mentions: users, mimetype: 'audio/mp4', fileName: `Hidetag.mp3` }, { quoted: m }) 
+ } else if ((isMedia && quoted.mtype === 'stickerMessage') && htextos) { 
+ var mediax = await quoted.download?.() 
+ conn.sendMessage(m.chat, {sticker: mediax, mentions: users}, { quoted: m }) 
+ } else { 
+ await conn.relayMessage(m.chat, {extendedTextMessage:{text: `${masss}\n${htextos}\n`, ...{ contextInfo: { mentionedJid: users, externalAdReply: { thumbnail: imagen1, sourceUrl: 'stocazzo' }}}}}, {}) 
+ }}} 
+ handler.command = /^(hidetag|notificar|menziona)$/i 
+ handler.group = true 
+ handler.admin = true 
+ handler.botAdmin = true 
+ export default handler
