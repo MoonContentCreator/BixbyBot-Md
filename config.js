9859f00f9a64cededae3a95799ff18c27d6bc6ce
MoonContentCreator <136917052+MoonContentCreator@users.noreply.github.com>
2023-10-24 16:22:50 +0200

    Update config.js

--- config.js
+++ config.js
@@ -56,14 +56,14 @@ global.imagen4 = fs.readFileSync('./bixbyvision16.png')
 //════════════ •⊰✧⊱• ════════════
 
 // Sticker WM
-global.packname = '𝐁𝐢𝐱𝐛𝐲𝐕𝐢𝐬𝐢𝐨𝐧𝐁𝐨𝐭'
+global.packname = '𝐁𝐢𝐱𝐛𝐲𝐁𝐨𝐭'
 global.author = '𝐌𝐝'
 
 //════════════ •⊰✧⊱• ════════════
 
 global.vs = '𝟏.𝟗'
 
-global.lb = 'ꪶ 𝐁𝐢𝐱𝐛𝐲 𝐕𝐢𝐬𝐢𝐨𝐧 ꫂ'
+global.nomebot = '𝐁𝐢𝐱𝐛𝐲𝐁𝐨𝐭-𝐌𝐝'
 
 global.multiplier = 69 
 global.maxwarn = '4' // máxima advertencias
