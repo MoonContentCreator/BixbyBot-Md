21288d238c3e47cb1be597cc13a85e635af861d6
MoonContentCreator <136917052+MoonContentCreator@users.noreply.github.com>
2024-03-18 14:51:38 +0100

    © BixbyBot-Md

--- plugins/owner-prefisso.js
+++ plugins/owner-prefisso.js
@@ -1,7 +1,7 @@
 let handler = async(m, { conn, text }) => {
-  if (!text) throw `𝐬𝐜𝐞𝐠𝐥𝐢 𝐮𝐧 𝐩𝐫𝐞𝐟𝐢𝐬𝐬𝐨`
+  if (!text) throw ` ⓘ 𝐒𝐜𝐞𝐠𝐥𝐢 𝐮𝐧 𝐩𝐫𝐞𝐟𝐢𝐬𝐬𝐨`
   global.prefix = new RegExp('^[' + (text || global.opts['prefix'] || '‎xzXZ/i!#$%+£¢€¥^°=¶∆×÷π√✓©®:;?&.\\-').replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&') + ']')
-  await m.reply(`𝐏𝐫𝐞𝐟𝐢𝐬𝐬𝐨 𝐜𝐚𝐦𝐛𝐢𝐚𝐭𝐨 𝐢𝐧: *${text}*`)
+  await m.reply(`ⓘ 𝐈𝐥 𝐩𝐫𝐞𝐟𝐢𝐬𝐬𝐨 𝐝𝐞𝐥 𝐛𝐨𝐭 𝐞' 𝐬𝐭𝐚𝐭𝐨 𝐜𝐚𝐦𝐛𝐢𝐚𝐭𝐨 𝐢𝐧: *${text}*`)
 }
 handler.help = ['setprefix'].map(v => v + ' [prefix]')
 handler.tags = ['owner']
