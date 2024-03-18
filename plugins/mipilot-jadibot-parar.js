50b19c41764b688d300b056a11bd1e7f9ccf870f
MoonContentCreator <136917052+MoonContentCreator@users.noreply.github.com>
2024-03-18 14:33:31 +0100

    © BixbyBot-Md

--- plugins/mipilot-jadibot-parar.js
+++ plugins/mipilot-jadibot-parar.js
@@ -1,9 +1,1 @@
-let handler  = async (m, { conn }) => {
-if (global.conn.user.jid == conn.user.jid) conn.reply(m.chat, 'ok', m)
-else {
-await conn.reply(m.chat, 'bye bot :\')', m)
-conn.ws.close()}}
-handler.command = /^(berhenti|stop)$/i
-handler.owner = true  
-handler.fail = null
-export default handler
+//
