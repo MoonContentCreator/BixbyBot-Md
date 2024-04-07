988b8cf6731817dfbbac76126801b0aff8faf46f
MoonContentCreator <136917052+MoonContentCreator@users.noreply.github.com>
2024-04-07 14:30:17 +0200

    Update and rename owner-prefisso to owner-resettaprefisso.js

--- plugins/owner-resettaprefisso.js
+++ plugins/owner-resettaprefisso.js
@@ -1,7 +1,8 @@
 let handler = async(m, { conn }) => {
 
   global.prefix = new RegExp('^[' + (opts['prefix'] || '‎xzXZ/i!#$%+£¢€¥^°=¶∆×÷π√✓©®:;?&.\\-').replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&') + ']')
-    await m.reply(`ⓘ 𝐈𝐥 𝐩𝐫𝐞𝐟𝐢𝐬𝐬𝐨 𝐝𝐞𝐥 𝐛𝐨𝐭 𝐞' 𝐬𝐭𝐚𝐭𝐨 𝐫𝐞𝐬𝐞𝐭𝐭𝐚𝐭𝐨`)
+    await m.reply(`ⓘ 𝐈𝐥 𝐩𝐫𝐞𝐟𝐢𝐬𝐬𝐨 𝐞' 𝐬𝐭𝐚𝐭𝐨 𝐫𝐞𝐬𝐞𝐭𝐭𝐚𝐭𝐨`)
+    // conn.fakeReply(m.chat, 'ⓘ 𝐈𝐥 𝐩𝐫𝐞𝐟𝐢𝐬𝐬𝐨 𝐞' 𝐬𝐭𝐚𝐭𝐨 𝐫𝐞𝐬𝐞𝐭𝐭𝐚𝐭𝐨', '0@s.whatsapp.net', 'Reset Prefix')
 }
 handler.help = ['resetprefix']
 handler.tags = ['owner']
