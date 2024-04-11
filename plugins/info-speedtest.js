df66646ccd966ed8a22f81eac20ed98bc31d4894
MoonContentCreator <136917052+MoonContentCreator@users.noreply.github.com>
2024-04-12 01:11:46 +0200

    Update plugins

---
+++ plugins/info-speedtest.js
@@ -0,0 +1,33 @@
+import cp from 'child_process';
+import { promisify } from 'util';
+import translate from '@vitalets/google-translate-api';
+const exec = promisify(cp.exec).bind(cp);
+
+const handler = async (m) => {
+  await conn.reply(m.chat, global.wait, m);
+  let o;
+  try {
+    o = await exec('python3 speed.py --secure --share');
+  } catch (e) {
+    o = e;
+  } finally {
+    const { stdout, stderr } = o;
+    if (stdout.trim()) {
+      const translated = await translate(stdout, { to: 'it' });
+      const text = translated.text
+        .replace(/(Scarica)/g, 'Download')
+        .replace(/(Carica)/g, 'Upload');
+      m.reply(text);
+    }
+    if (stderr.trim()) {
+      const translated = await translate(stderr, { to: 'it' });
+      m.reply(translated.text);
+    }
+  }
+};
+
+handler.help = ['speedtest'];
+handler.tags = ['info'];
+handler.command = /^(speedtest?|test?speed)$/i;
+
+export default handler;
