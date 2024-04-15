71c62cca67bf5d4b12462e9d587c33c50dc2e823
Daniel <136917052+MoonContentCreator@users.noreply.github.com>
2024-04-15 10:54:29 +0200

    Update info-funzionitotali.js

--- plugins/info-funzionitotali.js
+++ plugins/info-funzionitotali.js
@@ -1,6 +1,8 @@
 let handler = async (m) => {
     let funzionitotali = Object.values(global.plugins).filter(v => v.command).length;
-    m.reply(`ⓘ 𝐈𝐥 𝐛𝐨𝐭 𝐡𝐚 *${funzionitotali}*  𝐟𝐮𝐧𝐳𝐢𝐨𝐧𝐢.`);
+    let num1 = ['𝟏', '𝟐', '𝟑', '𝟒', '𝟓', '𝟔', '𝟕', '𝟖', '𝟗'];
+    let num2 = funzionitotali.toString().split('').map(digit => num1[digit]).join('');
+    m.reply(`ⓘ 𝐈𝐥 𝐛𝐨𝐭 𝐡𝐚 ${num2}  𝐟𝐮𝐧𝐳𝐢𝐨𝐧𝐢.`);
 }
 handler.command = ['funzionitotali', 'functions'];
 export default handler;
