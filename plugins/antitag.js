d7c21e0c78a7ba2cc103fccc7d907d72091a77c3
Daniel <136917052+MoonContentCreator@users.noreply.github.com>
2024-04-17 13:28:52 +0200

    Update antitag.js

--- plugins/antitag.js
+++ plugins/antitag.js
@@ -6,7 +6,7 @@ handler.before = async function (m, { conn, participants, groupMetadata }) {
     const listAdmin = groupAdmins.map(v => `- @${v.id.split('@')[0]}`).join('\n')
 
     if (m.mentionedJid.length >= 20) {
-        await conn.reply(m.chat, `> ⚠️ 𝐀𝐧𝐭𝐢-𝐓𝐚𝐠\n> ⓘ 𝐋'𝐮𝐭𝐞𝐧𝐭𝐞 𝐜𝐨𝐧 𝐭𝐚𝐠𝐚𝐥𝐥 𝐞' 𝐬𝐭𝐚𝐭𝐨 𝐫𝐢𝐦𝐨𝐬𝐬𝐨.\n${listAdmin}`, m, { mentions: [...groupAdmins.map(v => v.id), owner] });
+        await conn.reply(m.chat, `> ⚠️ 𝐀𝐧𝐭𝐢-𝐓𝐚𝐠\n> ⓘ 𝐋'𝐮𝐭𝐞𝐧𝐭𝐞 𝐜𝐨𝐧 𝐓𝐚𝐠-𝐀𝐥𝐥 𝐞' 𝐬𝐭𝐚𝐭𝐨 𝐫𝐢𝐦𝐨𝐬𝐬𝐨.\n${listAdmin}`, m, { mentions: [...groupAdmins.map(v => v.id), owner] });
         await conn.groupParticipantsUpdate(m.chat, [m.sender], "remove");
     }
 };
