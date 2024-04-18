e6455e515861a7bd25cb7601e85ec23b26e8db8b
Daniel <136917052+MoonContentCreator@users.noreply.github.com>
2024-04-18 02:22:34 +0200

    Update handler.js

--- handler.js
+++ handler.js
@@ -84,7 +84,7 @@ export async function handler(chatUpdate) {
                 if (!('antiTraba' in chat)) chat.antiTraba = true
                 if (!('antiArab' in chat)) chat.antiArab = true
                 if (!('modoadmin' in chat)) chat.modoadmin = false
-                if (!('antiPorn' in chat)) chat.antiPorn = true
+                if (!('antiPorno' in chat)) chat.antiPorno = true
                 if (!isNumber(chat.expired)) chat.expired = 0
                 if (!isNumber(chat.messaggi)) chat.messaggi = 0
                 if (!isNumber(chat.blasphemy)) chat.blasphemy = 0
@@ -117,7 +117,7 @@ export async function handler(chatUpdate) {
                     antiTraba: true,
                     antiArab: true,
                     modoadmin: false,
-                    antiPorn: true,
+                    antiPorno: true,
                     muto: false,
                     expired: 0,
                     messaggi: 0,
